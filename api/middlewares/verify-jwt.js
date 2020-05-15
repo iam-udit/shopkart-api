311deb92d4fe0d8847103749b14022c7558831db
udit6899 <udit6899@gmail.com>
2020-05-15 12:55:56 +0530

    Added authorization-status header to manage responses

--- api/middlewares/verify-jwt.js
+++ api/middlewares/verify-jwt.js
@@ -6,10 +6,12 @@ module.exports = function (req, res, next) {
     // Authenticating jwt token
     try {
         req.userData = jwt.verify(req.headers.authorization, process.env.JWT_SECRET_KEY);
+        res.setHeader("Authorization-Status", true);
         next();
     }
     // If authentication failed
     catch (error) {
+        res.setHeader("Authorization-Status", false);
         next(createError(401, "Authentication failed !"));
     }
 };
